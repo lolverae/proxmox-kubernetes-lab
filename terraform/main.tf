cef3880b88fccf4710d971f2c5cc35854d42111c
lolverae <alberto.olvera.espinosa@gmail.com>
2024-04-19 23:30:58 -0600

    Add ansible resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
 resource "proxmox_lxc" "debian_server" {
-  count = 1
+  count    = 1
   hostname = "debian-${count.index + 1}"
-  vmid = count.index + 3001
+  vmid     = count.index + 200
 
-  target_node = var.proxmox_node
-  ostemplate = "local:vztmpl/debian-12-standard_12.2-1_amd64.tar.zst"
-  password = var.ct_password
+  target_node  = var.proxmox_node
+  ostemplate   = "local:vztmpl/debian-12-standard_12.2-1_amd64.tar.zst"
+  password     = var.ct_password
   unprivileged = true
 
-  cores = 1
+  cores  = 1
   memory = 1024
 
   rootfs {
@@ -23,5 +23,24 @@ resource "proxmox_lxc" "debian_server" {
   }
 
   start = true
+}
+
+resource "ansible_host" "my_host" {
+  count  = 1
+  name   = proxmox_lxc.debian_server[count.index].vmid
+  groups = ["containers"]
+  variables = {
+    lxc_host      = proxmox_lxc.debian_server[count.index].vmid
+    physical_host = "pve1"
+  }
+}
+
+resource "ansible_host" "my_host2" {
+  name   = "pve1"
+  groups = ["hosts"]
+  variables = {
+    ansible_host = "http://20.0.0.38/"
+    ansible_user = "root"
+  }
 }
 
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,14 +1,18 @@
 terraform {
   required_providers {
     proxmox = {
-      source = "telmate/proxmox"
-      version = "2.9.14"
+      source  = "telmate/proxmox"
+      version = "3.0.1-rc1"
+    }
+    ansible = {
+      version = "~> 1.2.0"
+      source  = "ansible/ansible"
     }
   }
 }
 
 provider "proxmox" {
-  pm_api_url= var.proxmox_api_url
-  pm_api_token_id = var.proxmox_api_token_id
+  pm_api_url          = var.proxmox_api_url
+  pm_api_token_id     = var.proxmox_api_token_id
   pm_api_token_secret = var.proxmox_api_token_secret
 }
