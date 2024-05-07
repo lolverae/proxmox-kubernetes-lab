35f0262c70312b33ee3ad9d06a871a763498f4ba
lolverae <alberto.olvera.espinosa@gmail.com>
2024-05-06 21:24:02 -0600

    Split into modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,46 +1,27 @@
-resource "proxmox_lxc" "debian_server" {
-  count        = 1
-  hostname     = "debian-${count.index + 1}"
-  vmid         = count.index + 200
-  target_node  = var.proxmox_node
-  ostemplate   = "local:vztmpl/debian-12-standard_12.2-1_amd64.tar.zst"
-  password     = var.ct_password
-  unprivileged = true
-  cores        = 1
-  memory       = 1024
-
-  rootfs {
-    storage = "local-lvm"
-    size    = "5G"
-  }
-  network {
-    name   = "eth0"
-    bridge = "vmbr0"
-    ip     = "dhcp"
-  }
-
-  start = true
+module "debian_server" {
+  source = "./modules/proxmox"
+  server_name   = "debian"
+  os_template = "local:vztmpl/debian-12-standard_12.2-1_amd64.tar.zst"
+  server_count = 1
+  ct_password = var.ct_password 
+  proxmox_api_token_secret = var.proxmox_api_token_secret
+  proxmox_api_token_id = var.proxmox_api_token_id
+  proxmox_api_url = var.proxmox_api_url
 }
 
-resource "ansible_host" "debian_host" {
-  count  = 1
-  name   = proxmox_lxc.debian_server[count.index].vmid
-  groups = ["containers"]
-  variables = {
-    lxc_host      = proxmox_lxc.debian_server[count.index].vmid
-    physical_host = "pve1"
-  }
+module "alma_server" {
+  source = "./modules/proxmox"
+  server_name   = "alma"
+  os_template = "local:vztmpl/almalinux-9-default_20221108_amd64.tar.zst"
+  server_count = 1
+  ct_password = var.ct_password 
+  proxmox_api_token_secret = var.proxmox_api_token_secret
+  proxmox_api_token_id = var.proxmox_api_token_id
+  proxmox_api_url = var.proxmox_api_url
 }
 
-resource "ansible_playbook" "myplaybook" {
-  playbook   = "../ansible/update_all.yaml"
-  name       = proxmox_lxc.debian_server[0].hostname
-  replayable = true
-  extra_vars = {
-    ansible_user      = var.ansible_user
-    ansible_password  = var.ansible_password
-    ansible_sudo_pass = var.ansible_become_password
-  }
-
+module "ansible_debian" {
+  source = "./modules/ansible/"
+  ansible_password = var.ansible_password
+  ansible_become_password = var.ansible_become_password
 }
-
