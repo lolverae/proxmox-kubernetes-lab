b9a5f585d8bb705fc05aeb71859d614287a1ea80
lolverae <alberto.olvera.espinosa@gmail.com>
2024-05-13 22:19:06 -0600

    Use ansible provider groups

--- terraform/modules/ansible/main.tf
+++ terraform/modules/ansible/main.tf
@@ -1,14 +1,24 @@
 resource "ansible_host" "debian_host" {
   count  = var.server_count
   name   = var.server_id[count.index]
-  groups = ["containers"]
+  groups = ["debian_servers"]
   variables = {
     lxc_host      = var.server_id[count.index]
     physical_host = "pve1"
   }
 }
 
-resource "ansible_playbook" "myplaybook" {
+resource "ansible_host" "alma_host" {
+  count  = var.server_count
+  name   = var.server_id[count.index]
+  groups = ["almalinux_servers"]
+  variables = {
+    lxc_host      = var.server_id[count.index]
+    physical_host = "pve1"
+  }
+}
+
+resource "ansible_playbook" "testplaybook" {
   playbook   = "../../../ansible/update_all.yaml"
   name       = "Update playbook"
   replayable = true
@@ -17,6 +27,27 @@ resource "ansible_playbook" "myplaybook" {
     ansible_password  = var.ansible_password
     ansible_sudo_pass = var.ansible_become_password
   }
+}
 
+resource "ansible_playbook" "setupdebian" {
+  playbook   = "../../../ansible/setup_debian.yaml"
+  name       = "Setup Debian playbook"
+  replayable = true
+  extra_vars = {
+    ansible_user      = var.ansible_user
+    ansible_password  = var.ansible_password
+    ansible_sudo_pass = var.ansible_become_password
+  }
+}
+
+resource "ansible_playbook" "setupalma" {
+  playbook   = "../../../ansible/setup_alma.yaml"
+  name       = "Setup Alma playbook"
+  replayable = true
+  extra_vars = {
+    ansible_user      = var.ansible_user
+    ansible_password  = var.ansible_password
+    ansible_sudo_pass = var.ansible_become_password
+  }
 }
 
